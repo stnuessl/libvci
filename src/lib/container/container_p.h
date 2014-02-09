343c553afd34ae3c583d6660b8ff9a897eb98346
stnuessl <bockler@gmx.net>
2014-02-09 21:10:27 +0100

    Changed preprecessor macro to fit to the filename.

--- src/lib/container/container_p.h
+++ src/lib/container/container_p.h
@@ -1,9 +1,9 @@
-#ifndef _CONTAINER_UTIL_H_
-#define _CONTAINER_UTIL_H_
+#ifndef _CONTAINER_P_H_
+#define _CONTAINER_P_H_
 
 unsigned int adjust(unsigned int m, unsigned int min);
 
-#endif /* _CONTAINER_UTIL_H_ */
+#endif /* _CONTAINER_P_H_ */
 
 
 
